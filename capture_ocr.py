d58c4fa40946c44dd4d25fee90404d8fc77b5e6f
everyshare-code <138933190+everyshare-code@users.noreply.github.com>
2024-04-22 00:09:01 +0900

    update annotation

--- capture_ocr.py
+++ capture_ocr.py
@@ -4,10 +4,17 @@ import pyperclip
 import tkinter as tk
 from PIL import ImageGrab,Image,ImageFilter
 import io
+import time
 
 class ScreenCaptureTool(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        # 더블 클릭 방지를 위한 마지막 클릭 시간 기록
+        self.last_click_time = 0
+        # 더블 클릭 시간 간격
+        self.double_click_threshold = 0.3
+
 
         self.start_x = None
         self.start_y = None
@@ -21,6 +28,7 @@ class ScreenCaptureTool(tk.Tk):
         self.attributes("-alpha", 0.1)  # 투명도 조절
         self.attributes("-topmost", True)
         # self.overrideredirect(True) 윈도우 매니저 우회하여 창 만들기
+        # 창 사이즈 현재 기기의 스크린 사이즈로 설정
         self.geometry(f'{self.winfo_screenwidth()}x{self.winfo_screenheight()+200}')
         # self.attributes("-fullscreen", True)
 
@@ -28,22 +36,28 @@ class ScreenCaptureTool(tk.Tk):
         self.canvas.bind("<ButtonPress-1>", self.on_click)
         self.canvas.bind("<B1-Motion>", self.on_drag)
         self.canvas.bind("<ButtonRelease-1>", self.on_release)
-        # self.canvas.bind("<Escape>", lambda e: self.destroy())
+
         self.canvas.focus_set()
         self.canvas.bind("<KeyPress>", self.on_key_press)
 
     def on_click(self, event):
-        self.start_x = event.x
-        self.start_y = event.y
+        # 현재 클릭 시간
+        current_click_time = time.time()
+        # 현재 클릭 시간과 마지막 클릭한 시간이 더블 클릭 시간 간격보다 클 경우에만 동작
+        if (current_click_time - self.last_click_time) > self.double_click_threshold:
 
-        if not self.rect:
-            self.rect = self.canvas.create_rectangle(
-                self.start_x,
-                self.start_y,
-                self.start_x,
-                self.start_y,
-                outline="red",
-                width=2)
+            self.start_x = event.x
+            self.start_y = event.y
+
+            if not self.rect:
+                self.rect = self.canvas.create_rectangle(
+                    self.start_x,
+                    self.start_y,
+                    self.start_x,
+                    self.start_y,
+                    outline="red",
+                    width=2)
+        self.last_click_time = current_click_time
     def on_drag(self, event):
         self.canvas.coords(
             self.rect,
@@ -52,6 +66,7 @@ class ScreenCaptureTool(tk.Tk):
             event.x,
             event.y)
     def on_release(self, event):
+        # 드래그 방향에 따라 사각형이 만들어지는 위치가 다르기 때문에 min, max 함수 사용
         x1 = min(self.start_x, event.x)
         y1 = min(self.start_y, event.y)
         x2 = max(self.start_x, event.x)
@@ -65,13 +80,14 @@ class ScreenCaptureTool(tk.Tk):
     def on_esc_keypress(self,event):
         self.destroy()
     def capture_screen(self, x1, y1, x2, y2):
+        # 바운딩 박스 좌표
         bbox = (
             self.winfo_rootx() + x1,
             self.winfo_rooty() + y1,
             self.winfo_rootx() + x2,
             self.winfo_rooty() + y2
         )
-        # bbox = (x1, y1, x2, y2)
+        # 바운딩 박스 위치의 이미지 자르기
         screenshot = ImageGrab.grab(bbox)
         if screenshot.mode == 'RGBA':
             screenshot = screenshot.convert('RGB')
@@ -94,21 +110,24 @@ class ScreenCaptureTool(tk.Tk):
         content = Image.open(path)
         if content.mode == 'RGBA':
             content = content.convert('RGB')
+        # 이미지 선명도 조정
         sharpened = content.filter(ImageFilter.SHARPEN)
         byte_stream = io.BytesIO()
         sharpened.save(byte_stream, format='JPEG')
         byte_stream.seek(0)
 
+        # google vision에 맞는 이미지 객체 생성
         image = vision.Image(content=byte_stream.read())
 
         try:
+            # 해당 이미지에서 텍스트 추출
             response = client.text_detection(image=image)
             if response.full_text_annotation:
+                # 감지된 전체 텍스트
                 full_text = response.full_text_annotation.text
-                print("Full text:", full_text)
-                sentences = full_text.split('\n')
-                pyperclip.copy('\n'.join(sentences))
+                # 클립보드에 복사
+                pyperclip.copy(full_text)
+                # 캡처된 이미지 삭제
                 os.remove(path)
         except Exception as e:
             print("Error detecting text:", e)
-
